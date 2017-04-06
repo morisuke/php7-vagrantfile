44b5de53f38a65fccb7bc3d29e2b9d8e212aa958
morisuke <morisuke_tec@outlook.jp>
2017-04-07 01:34:12 +0900

    php version up

--- Vagrantfile
+++ Vagrantfile
@@ -6,8 +6,9 @@ Vagrant.configure(2) do |config|
   config.vm.box_url          = "https://github.com/2creatives/vagrant-centos/releases/download/v6.5.3/centos65-x86_64-20140116.box"
   config.vm.box_check_update = false
 
-  config.vm.network "forwarded_port", guest: 3306, host: 33061
+  config.vm.network "forwarded_port", guest: 3306, host: 33061, host_ip: "127.0.0.1"
   config.vm.network "private_network", ip: "192.168.33.115"
+  config.vm.network "public_network"
 
   # Virtual Box Settings
   config.vm.provider "virtualbox" do |vb|
@@ -25,7 +26,7 @@ Vagrant.configure(2) do |config|
 		"--natdnshostresolver1", "off"
 	]
   end
-  
+
   # provisioning
   config.vm.provision "shell", :path => "provision/basics.sh"
   config.vm.provision "shell", :path => "provision/php.sh"
--- provision/php.sh
+++ provision/php.sh
@@ -2,7 +2,7 @@
 
 : "PHP7" && {
 	rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-	yum --enablerepo=remi,remi-php70 install -y php php-devel php-mbstring php-zip php-bcmath php-pcntl php-ftp php-exif php-intl php-calendar php-sysvmsg php-sysvsem php-sysvshm php-wddx php-curl php-mcrypt php-iconv php-gmp php-pspell php-gd php-gd-native-ttf php-gd-jis-conv php-openssl php-pdo-mysql php-gettext php-zlib php-bz2 php-mysqli php-apxs2
+	yum --enablerepo=remi,remi-php71 install -y php php-devel php-mbstring php-zip php-bcmath php-pcntl php-ftp php-exif php-intl php-calendar php-sysvmsg php-sysvsem php-sysvshm php-wddx php-curl php-mcrypt php-iconv php-gmp php-pspell php-gd php-gd-native-ttf php-gd-jis-conv php-openssl php-pdo-mysql php-gettext php-zlib php-bz2 php-mysqli php-apxs2
 
 	: "composer" && {
 		curl -sS https://getcomposer.org/installer | php
